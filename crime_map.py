99607c9d70e012e496bfee5e87d1f850ecf52a74
Kajal Tiwari <kajal.lightbulb22@gmail.com>
2019-09-07 19:03:51 +0530

    Added exception catching

--- crime_map.py
+++ crime_map.py
@@ -16,8 +16,8 @@ def add():
     try:
         data = request.form.get('user_input')
         db.insert_data(data)
-    except:
-        print("Error: Inserting Data")
+    except Exception as e:
+        print(e)
     return home()
 
 
@@ -25,8 +25,8 @@ def add():
 def clear():
     try:
         db.delete_data()
-    except:
-        print("Error : Deleting Data")
+    except Exception as e:
+        print(e)
     return home()
 if __name__ == '__main__':
     app.run()
